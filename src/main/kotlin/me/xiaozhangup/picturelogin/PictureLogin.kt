073546901ce0c75d0044aedc51e74a633ee9c90d
Daniel Zhang <xiaozhangshiw@163.com>
2023-04-06 23:48:27 +0800

    To Taboo

---
+++ src/main/kotlin/me/xiaozhangup/picturelogin/PictureLogin.kt
@@ -0,0 +1,11 @@
+package me.xiaozhangup.picturelogin
+
+import taboolib.common.platform.Plugin
+import taboolib.common.platform.function.info
+
+object PictureLogin : Plugin() {
+
+    override fun onEnable() {
+        info("Successfully running ExamplePlugin!")
+    }
+}
